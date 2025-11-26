94ec5a6358592c9f52a716b3049e44ea8cc14e21
alex-zander-0403 <alexzander0403@gmail.com>
2025-11-27 01:21:55 +0300

    simple route-handlers file structire

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+  return (
+    <div className="flex justify-center items-center flex-col p-10 gap-5">
+      <h1 className="text-center text-3xl font-medium">
+        Страница пользователя (ProfilePage)
+      </h1>
+    </div>
+  );
+}
