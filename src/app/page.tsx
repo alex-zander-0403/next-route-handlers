8fd04cfb6d3e283ef60b2206af4a40737ea1e72b
alex-zander-0403 <alexzander0403@gmail.com>
2025-12-05 19:25:44 +0300

    feat: add UI for editing comments in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,22 @@ type Comment = {
 export default function Home() {
   const [comments, setComments] = useState<Comment[]>([]);
   const [newCommentText, setNewCommentText] = useState("");
+  const [editingId, setEditingId] = useState<string | null>(null);
+  const [editText, setEditText] = useState("");
 
+  // FN загрузка всех комментариев
   async function loadComments() {
+    // обращение к статическому ендпоинту get в comments -> route.ts
     const res = await fetch("/comments");
     const data = await res.json();
     setComments(data);
   }
 
+  // FN добавление комментария
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
 
+    // обращение к статическому ендпоинту post в comments -> route.ts
     const res = await fetch("/comments", {
       method: "POST",
       body: JSON.stringify({ text: newCommentText }),
@@ -28,6 +34,20 @@ export default function Home() {
 
     if (res.ok) {
       setNewCommentText("");
+    }
+  }
+
+  // FN редактирование комментария
+  async function handleUpdate(id: string) {
+    // обращение к динамическому ендпоинту PATCH в comments -> route.ts
+    const res = await fetch(`/comments/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({ text: editText }),
+    });
+
+    if (res.ok) {
+      setEditingId(null);
+      loadComments();
     }
   }
 
@@ -54,20 +74,44 @@ export default function Home() {
             className="px-3 py-2 rounded bg-gray-500"
           />
 
-          <button
-            type="submit"
-            // onClick={(e) => handleSubmit(e)}
-            className="p-3 rounded text-black bg-green-300"
-          >
+          <button type="submit" className="p-3 rounded text-black bg-green-300">
             Добавить
           </button>
         </form>
       </div>
 
       <ul className="flex flex-col p-5 gap-3 rounded bg-gray-600">
-        {comments.map((el) => (
-          <li key={el.id} className="px-10 py-3 rounded bg-gray-800">
-            <span>{el.text}</span>
+        {comments.map((comment) => (
+          <li key={comment.id} className="px-10 py-3 rounded bg-gray-800">
+            {editingId === comment.id ? (
+              <div>
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                  className="px-3 py-2 mr-5 rounded bg-gray-500"
+                />
+
+                <button onClick={() => handleUpdate(comment.id)}>
+                  Сохранить
+                </button>
+              </div>
+            ) : (
+              <div>
+                <span>{comment.text}</span>
+                <div>
+                  <button
+                    onClick={() => {
+                      setEditingId(comment.id);
+                      setEditText(comment.text);
+                    }}
+                    className="p-1 rounded text-black bg-blue-800"
+                  >
+                    Редактировать
+                  </button>
+                </div>
+              </div>
+            )}
           </li>
         ))}
       </ul>
