0a99e0ca20ff5e361847e8aca1e3f072769a385a
alex-zander-0403 <alexzander0403@gmail.com>
2025-12-08 01:05:02 +0300

    feat: create /users for query parameter

---
+++ src/app/users/mockUsersData.js
@@ -0,0 +1,17 @@
+export const users = [
+  {
+    id: 1,
+    name: "Alex",
+    email: "111@111",
+  },
+  {
+    id: 2,
+    name: "Bob",
+    email: "222@222",
+  },
+  {
+    id: 3,
+    name: "Carl",
+    email: "333@333",
+  },
+];
