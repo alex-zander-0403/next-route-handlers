e5e82df83faf61465a1218b3ec31118349bd1791
alex-zander-0403 <alexzander0403@gmail.com>
2025-12-08 09:48:05 +0300

    headers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,58 @@
+// import { type NextRequest } from "next/server";
+
+// //
+// export async function GET(request: NextRequest) {
+//   const requestHeaders = new Headers(request.headers);
+//   console.log(requestHeaders.get("Authorization")); // bearer 123qwerty123
+
+//   return new Response("Данные пользователя (route)");
+// }
+
+// import { headers } from "next/headers";
+
+// //
+// export async function GET() {
+//   const headerList = await headers();
+//   console.log(headerList.get("Authorization")); // bearer 123qwerty123
+
+//   return new Response("<h1>Данные пользователя (route)</h1>", {
+//     headers: { "Content-Type": "text/html; charset=utf-8" },
+//   });
+// }
+
+import { headers } from "next/headers";
+
 export async function GET() {
-  return new Response("Данные пользователя (route)");
+  const headerList = headers();
+  const acceptHeader = (await headerList).get("Accept");
+
+  const userData = {
+    name: "Alex",
+    email: "111@111",
+    role: "admin",
+  };
+
+  // определяем формат ответа
+  // если в заголовке "Accept" есть "application/json"
+  // возвращаем json
+  if (acceptHeader?.includes("application/json")) {
+    return new Response(JSON.stringify(userData), {
+      headers: { "Content-Type": "application/json; charset=UTF-8" },
+    });
+  } else {
+    // если нет
+    // возвращаем html
+    return new Response(
+      `<h1>Профиль пользователя:</h1>
+      <ul>
+        <li>${userData.name}</li>
+        <li>${userData.email}</li>
+        <li>${userData.role}</li>
+      </ul>
+      `,
+      {
+        headers: { "Content-Type": "text/html; charset=utf-8" },
+      }
+    );
+  }
 }
