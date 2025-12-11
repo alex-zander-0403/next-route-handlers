2d2ad359b871cc4b25e58ecb5be5fbb6f2541ae8
alex-zander-0403 <alexzander0403@gmail.com>
2025-12-12 00:10:13 +0300

    get/post cookies in fn

---
+++ src/app/theme/route.ts
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers";
+
+//
+export async function GET() {
+  const cookieStore = cookies();
+
+  const theme = (await cookieStore).get("theme")?.value || "light";
+
+  return Response.json({ theme });
+}
+
+//
+export async function POST(request: Request) {
+  const cookieStore = cookies();
+
+  const { theme } = await request.json();
+  (await cookieStore).set("theme", theme);
+
+  return Response.json({ theme });
+}
